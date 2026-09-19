daa4de36467d1565c033a351273cf1db49ac8625
Andre Luis Kunert <andreluiskunert@gmail.com>
2026-09-19 15:38:35 -0300

    Bora codar com JS_2026

---
+++ Mão_Na_Massa/Exercicios/fundamentosExercicios03.js
@@ -0,0 +1,14 @@
+//  Fundamentos 
+// Sábado_D'tarde, 12/09/2026
+console.log("FundamentosExercícios03")
+var minhaVariavel = "Good Afternoom_My Friend";
+console.log(minhaVariavel);
+console.log("Exerc01.")
+console.log("Loops & Arrays:")
+const nomes = ["Ana", "Joana", "Carlos",  "Mariane",  "Paola"]
+for (let i = 0; i < nomes.length; i++) {
+    console.log(nomes[i])
+    
+}
+console.log("===Exer02.=")
+console.log("The End")
